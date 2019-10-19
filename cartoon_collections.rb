0ca5ca8c34b9786dd85c7d2ef16d5ffd56760cd3
Logan Smith <logsmith06@gmail.com>
2019-10-19 21:59:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,13 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  counter = cheese_types.length
+  x = 0
+  counter.times do 
+  array.include?(cheese_types[x])
+  end
+  
 end
